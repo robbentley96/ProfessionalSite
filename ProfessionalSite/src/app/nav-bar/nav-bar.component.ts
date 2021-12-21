0497c7b1105b9bea37533f0c6459771abb602f5b
robbentley96 <Robert.Bentley@h-f.co.uk>
2021-12-21 19:40:38 +0000

    Website structure complete

--- ProfessionalSite/src/app/nav-bar/nav-bar.component.ts
+++ ProfessionalSite/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  ClickHomeButton(): void {
+    this.router.navigateByUrl('/');
+  }
+
+  ClickAcademicButton(): void {
+    this.router.navigateByUrl('/academic');
+  }
+
+  ClickEmploymentButton(): void {
+    this.router.navigateByUrl('/employment');
+  }
+
+  ClickProjectsButton(): void {
+    this.router.navigateByUrl('/projects');
+  }
 }
