93ad143cd122729a949d403f147ff99272e20849
robbentley96 <Robert.Bentley@h-f.co.uk>
2021-12-21 18:43:25 +0000

    Created routes

--- ProfessionalSite/src/app/app-routing.module.ts
+++ ProfessionalSite/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AcademicPageComponent } from './academic-page/academic-page.component';
+import { EmploymentPageComponent } from './employment-page/employment-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomePageComponent },
+  { path: "academic", component: AcademicPageComponent },
+  { path: "employment", component: EmploymentPageComponent },
+  { path: "projects", component: ProjectsPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
